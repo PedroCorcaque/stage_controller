722599f5eccc6719a2025cc913943b8d5f0c2c1a
PedroCorcaque <pedrollcorc@gmail.com>
2023-05-24 19:24:47 -0300

    Remove the monte carlo implementation

--- scripts/stage_controller.py
+++ scripts/stage_controller.py
@@ -3,10 +3,9 @@ import rospy
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-from tf.transformations import euler_from_quaternion
 import numpy as np
 
-class MonteCarloLocalization():
+class StageController():
 	
 	def __init__(self) -> None:
 
@@ -15,31 +14,20 @@ class MonteCarloLocalization():
 		rospy.Subscriber("/base_pose_ground_truth", Odometry, self.odometry_callback)
 		rospy.Subscriber("/base_scan", LaserScan, self.laser_scan_callback)
 
-		self.num_particles = 100
-		self.motion_noise = 0.1
-		self.scan_noise = 0.05
-
-		self.particles = np.random.rand(self.num_particles, 3)  # [x, y, theta]
-		self.weights = np.ones(self.num_particles) / self.num_particles
-
 		self.scan = None
-		self.estimated_pose = None
 
-		self.robot_position = np.zeros(3)  						# [x, y, theta]
-		self.target_position = np.array([0.0, 3.0])  			# [x, y]
+		self.robot_position = np.zeros(3)  						
+		self.target_position = np.array([3.0, 3.0])  			
 
-		self.linear_velocity = 0.1  							# Constant linear velocity
-		self.angular_velocity = 0.5  							# Constant angular velocity
-		self.kp_linear = 0.2  									# Proportional gain for linear velocity control
-		self.kp_angular = 1.0  									# Proportional gain for angular velocity control
-		self.min_distance = 0.5  								# Minimum distance threshold for obstacle avoidance
+		self.linear_velocity = 0.1  							
+		self.angular_velocity = 0.5  							
+		self.kp_linear = 0.2  									
+		self.kp_angular = 1.0  									
+		self.min_distance = 0.5  								
 
 	def laser_scan_callback(self, msg):
 		""" Callback function for laser scan """
 		self.scan = np.array(msg.ranges)
-		self.measurement_update()
-		self.resampling()
-		self.estimated_pose = np.average(self.particles[:, :2], axis=0, weights=self.weights)
 		self.move_to_target()
 
 	def odometry_callback(self, msg):
@@ -47,33 +35,6 @@ class MonteCarloLocalization():
 		self.robot_position[0] = msg.pose.pose.position.x
 		self.robot_position[1] = msg.pose.pose.position.y
 		self.robot_position[2] = 2 * np.arcsin(msg.pose.pose.orientation.z)
-		self.motion_update()
-
-	def motion_update(self):
-		""" Motion update """
-		delta_x = self.robot_position[0]
-		delta_y = self.robot_position[1]
-		delta_theta = self.robot_position[2]
-
-		self.particles[:, 0] += delta_x + np.random.randn(self.num_particles) * self.motion_noise
-		self.particles[:, 1] += delta_y + np.random.randn(self.num_particles) * self.motion_noise
-		self.particles[:, 2] += delta_theta + np.random.randn(self.num_particles) * self.motion_noise
-
-	def measurement_update(self):
-		""" Measurement update """
-		for i in range(self.num_particles):
-			particle_scan = self.scan  # Use actual laser scan data directly
-
-			diff = self.scan - particle_scan
-			self.weights[i] = np.exp(-np.sum(diff**2) / (2 * self.scan_noise**2))
-
-		self.weights /= np.sum(self.weights)  # Normalize weights
-
-	def resampling(self):
-		""" Resampling """
-		indices = np.random.choice(range(self.num_particles), size=self.num_particles, p=self.weights)
-		self.particles[:] = self.particles[indices]
-		self.weights.fill(1.0 / self.num_particles)
 
 	def move_to_target(self):
 		""" Move to target position """
@@ -109,5 +70,5 @@ class MonteCarloLocalization():
 
 if __name__ == "__main__":
     rospy.init_node("stage_controller")
-    robot = MonteCarloLocalization()
+    _ = StageController()
     rospy.spin()
